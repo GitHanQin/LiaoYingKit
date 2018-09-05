39b5e3b0571191468a2031dfd1aeb4bde8a6a2df
韩琴 <244629377@qq.com>
2018-09-05 12:39:26 +0800

    frist

---
+++ LiaoYingKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiaoYingKit
+//
+//  Created by 韩琴 on 2018/9/5.
+//  Copyright © 2018年 韩琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiaoYingKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiaoYingKit
+//
+//  Created by 韩琴 on 2018/9/5.
+//  Copyright © 2018年 韩琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
